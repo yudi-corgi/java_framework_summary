4a33a50f2293873d559a078e1b5aade25a022f03
YUDI-Corgi <1019509861@qq.com>
2023-07-01 18:43:20 +0800

    style: Modified consumer config.

--- summary-rocketmq/src/main/java/com/demo/allframework/rocketmq/consumer/Consumer.java
+++ summary-rocketmq/src/main/java/com/demo/allframework/rocketmq/consumer/Consumer.java
@@ -15,9 +15,13 @@ public class Consumer {
         // 初始化 Push 消费者，并指定消费者组，Broker 会主动推送消息给消费者
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("normal_consumer");
         // 指定 Namesrv 地址
-        consumer.setNamesrvAddr("119.91.138.217:9876");
+        consumer.setNamesrvAddr("localhost:9876");
         // 指定消费者订阅的主题，参二是子主题，即消息队列，若为 null || "" || "*" 表示全部订阅
         consumer.subscribe("SyncTopic", "");
+        // 指定每次拉取消息的数量，默认 32
+        // 服务端判断已读取消息数量默认最大也是 32，因此要突破该值，除了在 Consumer 设置
+        // 还需要在 broker 配置文件 broker.conf 指定参数 maxTransferCountOnMessageInMemory={number}
+        consumer.setPullBatchSize(10);
         // 设置消费者每次抓取消息的最大数量，默认 1
         consumer.setConsumeMessageBatchMaxSize(10);
         // 注册消息监听器以处理抓取的消息，此处采用并发消费，也可以为有序消费
