796b764123e46a39c850f033ddec15c0cb3e2123
YUDI-Corgi <chendongyu@kpaycn.local>
2024-06-26 17:36:19 +0800

    chore: Added test example.

--- summary-bareness/src/main/java/com/demo/allframework/bareness/service/impl/BizServiceImpl.java
+++ summary-bareness/src/main/java/com/demo/allframework/bareness/service/impl/BizServiceImpl.java
@@ -2,17 +2,32 @@ package com.demo.allframework.bareness.service.impl;
 
 import com.demo.allframework.bareness.entity.TestDTO;
 import com.demo.allframework.bareness.service.IBizService;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class BizServiceImpl implements IBizService {
 
+    private final Map<Long, TestDTO> DATA_SOURCE = new HashMap<>(16);
+
     @Override
-    public TestDTO select() {
-        return null;
+    public TestDTO select(Long id) {
+
+        System.out.println("Input param：" + id);
+
+        TestDTO result = DATA_SOURCE.get(id);
+        return result != null ? result : new TestDTO().setId(1L).setAmount(new BigDecimal("9.99"))
+                .setNumber(9527).setText("PASS").setCollection(List.of("nothing"));
     }
 
     @Override
-    public Long create() {
-        return 0L;
+    public Long create(TestDTO request) {
+        DATA_SOURCE.put(request.getId(), request);
+        return request.getId();
     }
 
     @Override
